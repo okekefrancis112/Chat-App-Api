0f6a9abef3b2da716ff01ebc15081d19e2941347
Francis Chukwudalu Okeke <okeke98@gmail.com>
2022-12-21 03:08:58 +0100

    Implemented chat

--- message_control/urls.py
+++ message_control/urls.py
@@ -1,10 +1,11 @@
 from rest_framework.routers import DefaultRouter
-from .views import GenericFiledUploadView
+from .views import GenericFiledUploadView, MessageView
 from django.urls import path, include
 
 router = DefaultRouter(trailing_slash=False)
 
 router.register("file-upload", GenericFiledUploadView)
+router.register("message", MessageView)
 
 urlpatterns = [
     path("", include(router.urls))
